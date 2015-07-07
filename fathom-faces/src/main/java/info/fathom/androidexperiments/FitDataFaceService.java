0828664e890e1761824ffb5233f73e0cd12e3a67
garciadelcastillo <personal@garciadelcastillo.es>
2015-07-07 14:15:55 -0400

    Test to implement Google Fit API on the watch face service, unsuccessful... :(

--- fathom-faces/src/main/java/info/fathom/androidexperiments/FitDataFaceService.java
+++ fathom-faces/src/main/java/info/fathom/androidexperiments/FitDataFaceService.java
@@ -5,6 +5,7 @@ import android.content.ContentUris;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.IntentSender;
 import android.database.Cursor;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -29,7 +30,24 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.Scopes;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.fitness.Fitness;
+import com.google.android.gms.fitness.data.DataPoint;
+import com.google.android.gms.fitness.data.DataSource;
+import com.google.android.gms.fitness.data.DataType;
+import com.google.android.gms.fitness.data.Field;
+import com.google.android.gms.fitness.data.Value;
+import com.google.android.gms.fitness.request.DataSourcesRequest;
+import com.google.android.gms.fitness.request.OnDataPointListener;
+import com.google.android.gms.fitness.request.SensorRequest;
+import com.google.android.gms.fitness.result.DataSourcesResult;
 
 import java.util.TimeZone;
 import java.util.concurrent.TimeUnit;
@@ -49,7 +67,14 @@ public class FitDataFaceService extends CanvasWatchFaceService implements Sensor
      */
     private static final long INTERACTIVE_UPDATE_RATE_MS = TimeUnit.SECONDS.toMillis(1);
 
-    // STEP SENSING
+
+
+//    ????????????????????????????????     ????????????????????   ??????????????????   ??? ???????
+//    ?????????????????????????????????    ?????????????????????  ???????????????????  ???????????
+//    ????????   ???   ??????  ????????    ??????????????  ?????? ???????????????????? ??????  ????
+//    ????????   ???   ??????  ???????     ??????????????  ??????????????????????????????????   ???
+//    ????????   ???   ???????????         ??????????????????? ???????????????????? ???????????????
+//    ????????   ???   ???????????         ???????????????????  ???????????????????  ????? ???????
     private SensorManager mSensorStepCountManager;
     private Sensor mSensorStepCount;
     private boolean mIsSensorStepCountRegistered;
@@ -69,6 +94,110 @@ public class FitDataFaceService extends CanvasWatchFaceService implements Sensor
             mStepsToday = Math.round(event.values[0]);
         }
     }
+
+
+
+
+//    ????????????????????     ?????? ??????? ???
+//    ????????????????????    ???????????????????
+//    ??????  ???   ???       ???????????????????
+//    ??????  ???   ???       ??????????????? ???
+//    ???     ???   ???       ???  ??????     ???
+//    ???     ???   ???       ???  ??????     ???
+    /**
+     * https://developers.google.com/fit/android/get-started#step_5_connect_to_the_fitness_service
+     */
+    private static final int REQUEST_OAUTH = 1;
+    /**
+     *  Track whether an authorization activity is stacking over the current activity, i.e. when
+     *  a known auth error is being resolved, such as showing the account chooser or presenting a
+     *  consent dialog. This avoids common duplications as might happen on screen rotations, etc.
+     */
+    private static final String AUTH_PENDING = "auth_state_pending";
+    private boolean authInProgress = false;
+    private GoogleApiClient mClient = null;
+
+    /**
+     *  Build a {@link GoogleApiClient} that will authenticate the user and allow the application
+     *  to connect to Fitness APIs. The scopes included should match the scopes your app needs
+     *  (see documentation for details). Authentication will occasionally fail intentionally,
+     *  and in those cases, there will be a known resolution, which the OnConnectionFailedListener()
+     *  can address. Examples of this include the user never having signed in before, or having
+     *  multiple accounts on the device and needing to specify which account to use, etc.
+     */
+    private void buildFitnessClient() {
+        Log.i(TAG, "Building Fitness Client");
+        // Create the Google API Client
+        mClient = new GoogleApiClient.Builder(this)
+                .addApi(Fitness.SENSORS_API)
+                .addScope(new Scope(Scopes.FITNESS_LOCATION_READ))
+                .addConnectionCallbacks(
+                        new GoogleApiClient.ConnectionCallbacks() {
+
+                            @Override
+                            public void onConnected(Bundle bundle) {
+                                Log.i(TAG, "Connected!!!");
+                                // Now you can make calls to the Fitness APIs.
+                                // Put application specific code here.
+                                Log.i(TAG, bundle.toString());
+                            }
+
+                            @Override
+                            public void onConnectionSuspended(int i) {
+                                // If your connection to the sensor gets lost at some point,
+                                // you'll be able to determine the reason and react to it here.
+                                if (i == ConnectionCallbacks.CAUSE_NETWORK_LOST) {
+                                    Log.i(TAG, "Connection lost.  Cause: Network Lost.");
+                                } else if (i == ConnectionCallbacks.CAUSE_SERVICE_DISCONNECTED) {
+                                    Log.i(TAG, "Connection lost.  Reason: Service Disconnected");
+                                }
+                            }
+                        }
+                )
+                .addOnConnectionFailedListener(
+                        new GoogleApiClient.OnConnectionFailedListener() {
+                            // Called whenever the API client fails to connect.
+                            @Override
+                            public void onConnectionFailed(ConnectionResult result) {
+                                Log.i(TAG, "Connection failed. Cause: " + result.toString());
+                                if (!result.hasResolution()) {
+                                    // Show the localized error dialog
+//                                    GooglePlayServicesUtil.getErrorDialog(result.getErrorCode(),
+//                                            FitDataFaceService.this, 0).show();
+                                    Log.e(TAG, "Error number: " +
+                                            GooglePlayServicesUtil.getErrorString(result.getErrorCode()));
+                                    return;
+                                }
+                                // The failure has a resolution. Resolve it.
+                                // Called typically when the app is not yet authorized, and an
+                                // authorization dialog is displayed to the user.
+                                if (!authInProgress) {
+//                                    try {
+                                    Log.i(TAG, "Attempting to resolve failed connection");
+                                    authInProgress = true;
+//                                        result.startResolutionForResult(MainActivity.this,
+//                                                REQUEST_OAUTH);
+//                                    } catch (IntentSender.SendIntentException e) {
+//                                        Log.e(TAG,
+//                                                "Exception while starting resolution activity", e);
+//                                    }
+                                } else {
+                                    Log.i(TAG, "Down here...");
+                                }
+                            }
+                        }
+                )
+                .build();
+    }
+
+
+
+
+
+
+
+
+
 
 
 
@@ -154,9 +283,24 @@ public class FitDataFaceService extends CanvasWatchFaceService implements Sensor
                     .setShowSystemUiTime(false)
                     .build());
 
+            /**
+             * STEP SENSING
+             */
             mSensorStepCountManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
             mSensorStepCount = mSensorStepCountManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
             // REGISTERING MOVED TO onVisibilityChanged
+
+            /**
+             * FIT API
+             */
+            // this doesn't work, since the Engine.onCreate method works differently than an activity
+//            if (savedInstanceState != null) {
+//                authInProgress = savedInstanceState.getBoolean(AUTH_PENDING);
+//            }
+
+            authInProgress = true;
+
+            buildFitnessClient();
 
             mTextPaintInteractive = new Paint();
             mTextPaintInteractive.setColor(TEXT_COLOR_INTERACTIVE);
